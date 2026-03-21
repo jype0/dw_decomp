ada465f55e397e341e0f7ca48b19ea53901be400
Pekka Jylhä-Ollila <pekka.jylha.ollila@gmail.com>
2026-03-21 22:44:27 +0200

    Move fade variable declarations to header

--- include/dw/fade.h
+++ include/dw/fade.h
@@ -1,6 +1,19 @@
 #ifndef DW_FADE_H
 #define DW_FADE_H
 
+#include <dw/types.h>
+
+#pragma section sbss begin
+extern int16_t FADE_OUT_TARGET;
+extern int16_t FADE_IN_TARGET;
+extern int16_t FADE_OUT_CURRENT;
+extern int16_t FADE_IN_CURRENT;
+extern uint8_t FADE_PROGRESS;
+extern uint8_t FADE_MODE;
+extern uint16_t FADE_PADDING;
+extern int32_t FADE_PROTECTION;
+#pragma section sbss end
+
 void initializeFadeData(void);
 
 #endif
--- src/main/fade.c
+++ src/main/fade.c
@@ -1,19 +1,8 @@
-#include <dw/types.h>
+#include <dw/fade.h>
 
 #include "common.h"
 
 #pragma optimization_level 4
-
-#pragma section sbss begin
-extern int16_t FADE_OUT_TARGET;
-extern int16_t FADE_IN_TARGET;
-extern int16_t FADE_OUT_CURRENT;
-extern int16_t FADE_IN_CURRENT;
-extern uint8_t FADE_PROGRESS;
-extern uint8_t FADE_MODE;
-extern uint16_t FADE_PADDING;
-extern int32_t FADE_PROTECTION;
-#pragma section sbss end
 
 void initializeFadeData(void)
 {
