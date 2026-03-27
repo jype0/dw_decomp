dccc73330123e3e36922b1d9da6ccd35a5e6da72
SydMontague <sydmontague@phoenix-staffel.de>
2026-03-27 15:40:55 +0100

    Added evolution.c

--- include/dw/evl.h
+++ include/dw/evl.h
@@ -30,17 +30,42 @@ typedef struct {
 } EvoSequenceData;
 
 typedef struct {
-	int16_t targetDigimon;
 	int16_t hp;
 	int16_t mp;
 	int16_t offense;
 	int16_t defense;
 	int16_t speed;
 	int16_t brains;
+	int16_t targetDigimon;
 } EvoStatsGains;
+
+typedef struct {
+  int8_t from[5];
+  int8_t to[6];
+} EvolutionPath;
+
+typedef struct {
+  int16_t digimon;
+  int16_t hp;
+  int16_t mp;
+  int16_t offense;
+  int16_t defense;
+  int16_t speed;
+  int16_t brain;
+  int16_t care;
+  int16_t weight;
+  int16_t discipline;
+  int16_t happiness;
+  int16_t battles;
+  int16_t techs;
+  int8_t flags;
+  uint8_t padding;
+} EvoRequirements;
 
 extern EvoSequenceData EVO_SEQUENCE_DATA;
 
 extern EvoStatsGains EVO_GAINS_DATA[];
+extern EvolutionPath EVO_PATHS_DATA[62];
+extern EvoRequirements EVO_REQ_DATA[63];
 
 #endif
